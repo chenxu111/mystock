a1f45be387cd3a99d5c61525ebaabf2eeb0c941e
chenxu111 <chenxu111@gmail.com>
2014-02-25 19:47:47 +0800

    add log4j

--- src/org/freemoney/db/DataBaseManager.java
+++ src/org/freemoney/db/DataBaseManager.java
@@ -3,7 +3,9 @@ package org.freemoney.db;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -42,38 +44,118 @@ public class DataBaseManager {
 			throw new ExceptionInInitializerError("databasepool init error");
 		}
 	}
-	
+
 	/**
-	 *  从连接池获取一个连接
+	 * 从连接池获取一个连接
+	 * 
 	 * @return
 	 */
-	public static Connection getConnection()
-	{
-		try
-		{
+	public static Connection getConnection() {
+		try {
 			Connection conn = ds.getConnection();
 			return conn;
-		}catch(Exception e)
-		{
+		} catch (Exception e) {
 			log.info("getConnection fail:", e);
 		}
 		return null;
 	}
-	
+
 	/**
 	 * 释放连接
+	 * 
 	 * @param conn
 	 */
-	public static void releaseConnection(Connection conn)
-	{
+	public static void releaseConnection(Connection conn) {
 		try {
-			if(conn != null && !conn.isClosed())
-			{
+			if (conn != null && !conn.isClosed()) {
 				conn.close();
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			log.info("releaseConnection fail",e);
+			log.info("releaseConnection fail", e);
 		}
+	}
+
+	/**
+	 * 关闭statement
+	 * @param stmt
+	 * @param resultSet
+	 * @throws SQLException
+	 */
+	public static void closeStatement(Statement stmt, ResultSet resultSet)
+			throws SQLException {
+		try {
+			if (resultSet != null) {
+				resultSet.close();
+			}
+		} catch (SQLException e) {
+			log.error("closeStatment error", e);
+			e.printStackTrace();
+		}
+
+		try {
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			log.error("closeStatement fail:", e);
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * 关闭statement
+	 * @param stmt
+	 * @throws SQLException
+	 */
+	public static void closeStatement(Statement stmt) {	
+		try {
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			log.error("closeStatement fail:", e);
+			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet executeQuery(String sql)
+	{
+		ResultSet rs = null;
+		Statement _stmt = null;
+		
+		Connection conn = getConnection();
+		
+		try {
+			_stmt = conn.createStatement();
+			rs = _stmt.executeQuery(sql);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			log.error("executequery fail:",e);
+			e.printStackTrace();
+		}finally{
+			closeStatement(_stmt);
+			releaseConnection(conn);
+		}
+		return rs;
+	}
+	
+	public void updateDb(String sql)
+	{
+		Connection conn = getConnection();
+		Statement _stmt = null;
+		try {
+			_stmt = conn.createStatement();
+			_stmt.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			log.error("updateDb",e);
+			e.printStackTrace();
+		}finally{
+			closeStatement(_stmt);
+			releaseConnection(conn);
+		}
+		
+		
 	}
 }
